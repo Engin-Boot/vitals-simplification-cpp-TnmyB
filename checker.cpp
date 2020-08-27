e18d4e7be4ac6ed368548873280e8fc02d7e02f8
TnmyB <68606780+TnmyB@users.noreply.github.com>
2020-08-27 12:35:52 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,22 @@
 #include <assert.h>
 #include <limits.h>
 
-bool vitalInRange(float value, float lower_limit = INT_MIN, float upper_limit = INT_MAX){
+bool vitalInRange(float value, float lower_limit, float upper_limit){
 
     return !(value < lower_limit || value > upper_limit);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   
-  return vitalInRange(bpm,70,150) && vitalInRange(spo2,90) && vitalInRange(respRate,30,95);
+  return vitalInRange(bpm,70,150) && vitalInRange(spo2,90,100) && vitalInRange(respRate,30,95);
 }
 
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
-  assert(vitalsAreOk(70, 90, 30) == true);
-  assert(vitalsAreOk(150, 200, 95) == true);
-  assert(vitalsAreOk(69, 90, 30) == false);
-  assert(vitalsAreOk(-1, -1, -1) == false);
-  assert(vitalsAreOk(200, 200, 200) == false);
+ 
+  assert(vitalInRange(60,70,150) == false);
+  assert(vitalInRange(70, 70, 150) == true);
+  assert(vitalInRange(160, 70, 150) == false);
+  assert(vitalInRange(75, 70, 150) == true);
 }
