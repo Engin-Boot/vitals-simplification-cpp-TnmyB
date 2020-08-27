454f436cb07258e43d5758d38df0774966c8d33f
TnmyB <68606780+TnmyB@users.noreply.github.com>
2020-08-27 11:08:21 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,19 +2,20 @@
 
 
 bool check_bpm(float bpm){
-    return (bpm>=70 && bpm<=150);
+    return !(bpm < 70 || bpm > 150);
 }
 
 bool check_spo2(float spo2){
-    return (spo2>=90);
+    return !(spo2 < 90);
 }
 
 bool check_respRate(float respRate){
-    return (respRate>=70 && respRate<=95);
+    return !(respRate < 30 || respRate > 95);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return check_bpm(bpm)&&check_spo2(spo2)&&check_respRate(respRate);
+  
+  return (check_bpm(bpm)&&check_spo2(spo2)&&check_respRate(respRate));
 }
 
 int main() {
