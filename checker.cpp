e10bee7f1d4bf06ddc2a168f54469affb995cd6e
Tanmay <68606780+TnmyB@users.noreply.github.com>
2020-08-30 20:38:20 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,94 @@
-#include <assert.h>
-#include <limits.h>
+#include <iostream>
+using namespace std;
 
-bool vitalInRange(float value, float lower_limit, float upper_limit){
+class AlertInterface
+{ 
 
-    return !(value < lower_limit || value > upper_limit);
-}
+    public:
+       virtual void raiseAlert(const char* vitalName, const char* level)=0;
+};
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  
-  return vitalInRange(bpm,70,150) && vitalInRange(spo2,90,100) && vitalInRange(respRate,30,95);
-}
+class AlertWithSMS: public AlertInterface{
+    public:
+        void raiseAlert(const char* vitalName, const char* level){
+            std::cout<<"SMS: "<<vitalName<<" "<<level;
+        }
+};
 
-int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
- 
-  assert(vitalInRange(60,70,150) == false);
-  assert(vitalInRange(70, 70, 150) == true);
-  assert(vitalInRange(160, 70, 150) == false);
-  assert(vitalInRange(75, 70, 150) == true);
-}
+class AlertWithSound: public AlertInterface{
+    public:
+        void raiseAlert(const char* vitalName, const char* level){
+            std::cout<<"Sound!"<<vitalName<<" "<<level;
+        }
+};
+
+class vital_info{
+    public:
+     float lowerLimit;
+     float upperLimit;
+     float value;
+     const char* name;
+     AlertInterface* alerter;
+
+    public:
+
+    void vital_initialise(const char* name, float lowerLimit, float upperLimit,AlertInterface* alerter){
+        this->name=name;
+        this->lowerLimit=lowerLimit;
+        this->upperLimit=upperLimit;
+        this->alerter=alerter;
+    }
+
+    const char* check(int val){
+        if(val<lowerLimit){
+            return "too low";
+        }
+        else if(val>upperLimit){
+            return "too high";
+        }
+        else{
+            return "is ok";
+        }
+    }
+};
+
+class VitalsChecker
+{
+    private:
+      int* value;
+      vital_info* vitals;
+
+     
+    public:
+      VitalsChecker(vital_info list[]){
+          this->vitals = list;
+      }
+
+      void VitalValue(int* v){
+        value=v;
+        checkAndAlert();        
+      }
+
+      void checkAndAlert(){
+        for(int i=0;i<3;i++){
+           vitals[i].alerter->raiseAlert(vitals[i].name, vitals[i].check(value[i]));
+        }    
+      }
+};
+
+int main(){
+    //initialize all vitals, type of alarm ,required
+    vital_info vital_list[3];
+    AlertWithSMS SMS;
+    AlertWithSound sound;
+    //input can be changed/added
+    vital_list[0].vital_initialise("pulse",70,150,&SMS);
+    vital_list[1].vital_initialise("spo2",90,100,&sound);
+    vital_list[2].vital_initialise("pulse",30,95,&sound);
+     
+    //Input the vital measurements
+    VitalsChecker check(vital_list);
+    int val[]={80,95,60};
+    check.VitalValue(val);
+    return 0;
+} 
